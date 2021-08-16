40cd34fc1b22744146f5b5631d39ffb13713669e
Marko <goncarmarko@gmail.com>
2021-08-16 16:51:40 +0300

    add xplugd

--- .dotfiles/scripts/setup/bspwm/setup.sh
+++ .dotfiles/scripts/setup/bspwm/setup.sh
@@ -4,6 +4,6 @@ DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 sudo xbps-install -S dex bspwm sxhkd polybar dunst rofi i3lock-color yad \
                      qjackctl pamixer pavucontrol \
                      hsetroot kvantum qt5ct papirus-icon-theme \
-                     playerctl light numlockx lxqt-policykit wmname redshift redshift-gtk rofi-emoji
+                     playerctl light numlockx lxqt-policykit xplugd xss-lock wmname redshift redshift-gtk rofi-emoji
 
 "$DIR"/themes.sh
