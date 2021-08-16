37f61d73c13f52a2d6d20b44f3bb308f650e7c67
Marko <goncarmarko@gmail.com>
2021-08-16 09:13:52 +0300

    gu moment

--- .dotfiles/scripts/setup/nvidia/nvidia.sh
+++ .dotfiles/scripts/setup/nvidia/nvidia.sh
@@ -3,5 +3,5 @@ DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 
 sudo xbps-install -S void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 sudo xbps-install -S nvidia nvidia-libs nvidia-libs-32bit linux-firmware-nvidia \
-                     vulkan-loader vulkan-loader-32bit vulkan-tools vulkan-validationlayers vulkan-validationlayers-32bit \
+                     vulkan-loader vulkan-loader-32bit Vulkan-Tools Vulkan-ValidationLayers Vulkan-ValidationLayers-32bit \
                      glxinfo mesa-demos
