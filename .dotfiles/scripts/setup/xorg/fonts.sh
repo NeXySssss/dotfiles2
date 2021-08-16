b56c9cca2ec37987f2d5410c27e3510fc15f7840
Marko <goncarmarko@gmail.com>
2021-08-16 09:21:40 +0300

    fix fonts

--- .dotfiles/scripts/setup/xorg/fonts.sh
+++ .dotfiles/scripts/setup/xorg/fonts.sh
@@ -3,8 +3,8 @@ DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 
 sudo xbps-install -S curl jq unzip noto-fonts-ttf noto-fonts-cjk
 
-"$DIR"/../fonts/codicons.sh
-"$DIR"/../fonts/jetbrainsmono.sh
-"$DIR"/../fonts/nerdfonts.sh JetBrainsMono
+"$DIR"/../../fonts/codicons.sh
+"$DIR"/../../fonts/jetbrainsmono.sh
+"$DIR"/../../fonts/nerdfonts.sh JetBrainsMono
 
 fc-cache
