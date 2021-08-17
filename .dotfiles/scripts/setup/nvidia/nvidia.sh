d538f3d40efd90f8be340bd083d481e4fd555a4f
Marko <goncarmarko@gmail.com>
2021-08-18 01:00:35 +0300

    add more 32bit graphics libs

--- .dotfiles/scripts/setup/nvidia/nvidia.sh
+++ .dotfiles/scripts/setup/nvidia/nvidia.sh
@@ -2,6 +2,6 @@
 DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 
 sudo xbps-install -S void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
-sudo xbps-install -S mesa mesa-dri mesa-32bit mesa-vaapi mesa-vdpau mesa-demos glxinfo \
+sudo xbps-install -S mesa mesa-dri mesa-32bit mesa-dri-32bit mesa-vaapi mesa-vaapi-32bit mesa-vdpau mesa-vdpau-32bit mesa-demos glxinfo \
                      vulkan-loader vulkan-loader-32bit Vulkan-Tools Vulkan-ValidationLayers Vulkan-ValidationLayers-32bit \
                      linux-firmware-nvidia nvidia nvidia-libs nvidia-libs-32bit
