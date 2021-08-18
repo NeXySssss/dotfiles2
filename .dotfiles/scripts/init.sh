914e4e0284779fa365db5bc91e5708382e9103b3
Marko <goncarmarko@gmail.com>
2021-08-18 18:43:29 +0300

    add -y to init script

--- .dotfiles/scripts/init.sh
+++ .dotfiles/scripts/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-sudo xbps-install -S git
+sudo xbps-install -Sy git
 
 git clone --bare https://github.com/33kk/dotfiles "$HOME"/.dotfiles/git
 git --bare --git-dir="$HOME"/.dotfiles/git --work-tree="$HOME" checkout
