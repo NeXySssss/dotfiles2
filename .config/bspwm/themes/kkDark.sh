07b4e18f10e58e79da3c788d361d84f22648a53d
Marko <goncarmarko@gmail.com>
2021-08-24 03:29:27 +0300

    make bspwm and kitty easier to theme

---
+++ .config/bspwm/themes/kkDark.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+bspc config border_width 1
+bspc config window_gap 0
+bspc config top_padding 30
+bspc config bottom_padding 0
+bspc config left_padding 0
+bspc config right_padding 0
+
+bspc config normal_border_color "#111111"
+bspc config active_border_color "#111111"
+bspc config focused_border_color "#9E9E9E"
+bspc config presel_feedback_color "#9E9E9E"
