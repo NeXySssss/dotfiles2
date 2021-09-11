5c7ca4e3d6deb9942815a43943c1ffdbb105c226
Marko <goncarmarko@gmail.com>
2021-09-11 10:26:59 +0300

    shorten

--- .config/nvim/lua/plugins/dial.lua
+++ .config/nvim/lua/plugins/dial.lua
@@ -1,8 +1,10 @@
 local u = require("utils")
 
-u.map("n", "<C-a>", "<Plug>(dial-increment)", { silent = true })
-u.map("n", "<C-x>", "<Plug>(dial-decrement)", { silent = true })
-u.map("v", "<C-a>", "<Plug>(dial-increment)", { silent = true })
-u.map("v", "<C-x>", "<Plug>(dial-decrement)", { silent = true })
-u.map("v", "g<C-a>", "<Plug>(dial-increment-additional)", { silent = true })
-u.map("v", "g<C-x>", "<Plug>(dial-decrement-additional)", { silent = true })
+local s = { silent = true }
+
+u.map("n", "<C-a>", "<Plug>(dial-increment)", s)
+u.map("n", "<C-x>", "<Plug>(dial-decrement)", s)
+u.map("v", "<C-a>", "<Plug>(dial-increment)", s)
+u.map("v", "<C-x>", "<Plug>(dial-decrement)", s)
+u.map("v", "g<C-a>", "<Plug>(dial-increment-additional)", s)
+u.map("v", "g<C-x>", "<Plug>(dial-decrement-additional)", s)
