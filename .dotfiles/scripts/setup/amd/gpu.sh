702c78acd95669ef138603541c6a3df01ca18ef4
NeXySssss <68652421+NeXySssss@users.noreply.github.com>
2022-09-22 00:40:20 +0700

    ....
    FDM

--- .dotfiles/scripts/setup/amd/gpu.sh
+++ .dotfiles/scripts/setup/amd/gpu.sh
@@ -6,9 +6,9 @@ log_info "Installing packages"
 case "$DISTRO" in
 	void)
 		sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree || error "Failed to enable repos"
-		sudo xbps-install -Sy linux-firmware-amd xf86-video-amdgpu mesa-vulkan-radeon mesa-vulkan-radeon-32bit amdvlk amdvlk-32bit || error "Failed to install packages"
+		sudo xbps-install -Sy linux-firmware-amd xf86-video-amdgpu mesa-vulkan-radeon mesa-vulkan-radeon-32bit || error "Failed to install packages"
 	;;
 	arch)
-		sudo pacman --needed --noconfirm  -Syu xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk libva-mesa-driver mesa-vdpau || error "Failed to install packages"
+		sudo pacman --needed --noconfirm  -Syu xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon libva-mesa-driver mesa-vdpau || error "Failed to install packages"
 	;;
 esac
